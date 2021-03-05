62a2f8fcc551df9fc97bf377ff11c10455fbe6ac
KondratevProgi <79056993+KondratevProgi@users.noreply.github.com>
2021-03-06 01:01:11 +0300

    Lab3
    Задание 1, изменено
    Задание 2, max(X,Y,Z,U).

--- Lab3.pl
+++ Lab3.pl
@@ -1,2 +1,5 @@
-max(X,Y,Z):-X>Y,Z is X.
-max(X,Y,Z):-Y>X,Z is Y.
+max(X,Y,Z):-X>Y,Z is X,!.
+max(X,Y,Z):-Y>X,Z is Y,!.
+max(X,Y,Z,U):-X>Y,X>Z,U is X,!.
+max(_,Y,Z,U):-Y>Z,U is Y,!.
+max(_,_,Z,U):-U is Z,!.
