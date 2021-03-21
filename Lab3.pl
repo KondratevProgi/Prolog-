9e7e9ff87b777be971678da22097547055ad5411
KondratevProgi <79056993+KondratevProgi@users.noreply.github.com>
2021-03-21 16:47:31 +0300

    Lab3
    Nomer 4

--- Lab3.pl
+++ Lab3.pl
@@ -1,7 +1,20 @@
+%Nomer #1
 max(X,Y,Z):-X>Y,Z is X,!.
 max(X,Y,Z):-Y>X,Z is Y,!.
+
+%Nomer #2
 max(X,Y,Z,U):-X>Y,X>Z,U is X,!.
 max(_,Y,Z,U):-Y>Z,U is Y,!.
 max(_,_,Z,U):-U is Z,!.
+
+%Nomer #3
+%Recursion up
 fact(1,1):-!.
 fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
+
+%Nomer #4
+%Recursion down
+fact(1,ConX,ConX):-!.
+fact(N,ConX,X):-ConX1 is ConX*N, N1 is N-1,fact(N1,ConX1,X).
+factd(N,X):-fact(N,1,X).
+
