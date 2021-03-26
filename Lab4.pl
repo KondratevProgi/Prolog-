5625391b3a3bbc2714937cb510e4f2e221f12d7b
KondratevProgi <79056993+KondratevProgi@users.noreply.github.com>
2021-03-26 23:12:43 +0300

    Lab4
    Nomer 2

--- Lab4.pl
+++ Lab4.pl
@@ -1,5 +1,17 @@
 %Nomer 1
-append_ch([],X,X).
-append_ch([H|T1],X,[H|T2]):-append_ch(T1,X,T2).
+append_list([],X,X).
+append_list([H|T1],X,[H|T2]):-append_list(T1,X,T2).
+
+read_list(0,[]):-!.
+read_list(N,X):-read(X1),N1 is N-1,read_list(N1,X2),append_list([X1],X2,X).
+
+write_list([X]):-write(X).
+write_list([X|Y]):-write(X),write_list(Y).
+
+%Nomer 2
+sum_list_down(Y,Sum):-sum_list_down(Y,0,Sum).
+sum_list_down([],Sum,Sum):-!.
+sum_list_down([H|T],Suma,Sum):-Suma1 is Suma+H,sum_list_down(T,Suma1,Sum).
+sumat_list_down(N,Sum):-read_list(N,X),sum_list_down(X,Sum).
 
 
