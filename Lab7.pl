3c02b371596b34eaa651451c8eb52a1319110682
KondratevProgi <79056993+KondratevProgi@users.noreply.github.com>
2021-04-21 20:59:36 +0300

    Lab7
    Nomer2

--- Lab7.pl
+++ Lab7.pl
@@ -14,3 +14,24 @@ nomer1 :- write("Vvedite stroky:"),read_stroka_neopr(S),
           write_stroka(S),write(","),write_stroka(S), write(","),
           write_stroka(S),nl,write("Kolvo elem: "),
           count_elem(S, Kol),write(Kol).
+
+%Nomer 2
+stroka_no1probel([],[]):- !.
+stroka_no1probel([H|T],[H|T]):- H\=32,!.
+stroka_no1probel([_|T],Str):- stroka_no1probel(T,Str).
+
+do1probel([],Slov,Slov):- !.
+do1probel([H|_],Slov,Slov):- H=32,!.
+do1probel([H|T],KSlov,NSlov):-append(KSlov,[H],KSlov1),do1probel(T,KSlov1,NSlov).
+do1probel(Str,Slov):- do1probel(Str,[],Slov).
+
+kol_slovnopro([],Kol,Kol):- !.
+kol_slovnopro(Str,KKol,Kol):-stroka_no1probel(Str,Probel),(Probel\=[]->
+           (do1probel(Probel,Slov),append(Slov,Slov1,Probel),
+           KKol1 is KKol+1);(Slov1=[],KKol1 is KKol)),
+           count_words(Slov1,KKol1,Kol).
+kol_slovnopro(Str,Kol):- kol_slovnopro(Str,0,Kol).
+
+nomer2 :- write("Vvedite stroky: "),read_stroka_neopr(S),write("Kolvo slov: "),
+          kol_slovnopro(S,Kol),write(Kol).
+
